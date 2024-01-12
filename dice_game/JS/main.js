e73c8eef523a83133b7a3b99c56c3777dc4e403b
jonathanlombardo <jonathanlombardo90@gmail.com>
2024-01-12 17:13:14 +0100

    style: dice

--- dice_game/JS/main.js
+++ dice_game/JS/main.js
@@ -18,8 +18,14 @@ diceButton.addEventListener("click", function () {
     winner = "Patta!";
   }
 
-  playerDiceElement.innerText = `PLAYER: ${playerDice}`;
-  cpuDiceElement.innerText = `CPU: ${cpuDice}`;
+  cpuDiceElement.innerHTML = `
+  <h2 class="dice-label text-danger">CPU</h2>
+  <i class="bi bi-dice-${cpuDice}-fill text-danger">
+  `;
+  playerDiceElement.innerHTML = `
+  <h2 class="dice-label text-primary">Player</h2>
+  <i class="bi bi-dice-${playerDice}-fill text-primary">
+  `;
   resultElement.innerText = winner;
   resultElement.classList.remove("d-none");
 });
